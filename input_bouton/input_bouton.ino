6d72de8e2f5e2e1f25d6fed254b824a1a857fcd5
Ben40170 <benoit_campagne@me.com>
2017-01-04 12:41:20 +0100

    Optimisation des var delay

--- input_bouton/input_bouton.ino
+++ input_bouton/input_bouton.ino
@@ -7,13 +7,12 @@ int capteur, led, result, maxValue;
 // Paramètres
 //
 int seuil = 100; // seuil de déclenchement
-int ledTime = 10; // temps d'allumage de la LED en ms
 int refreshDelay = 1; // Delais de raffraichissement de la boucle
 int pinNumberLed = 13; // numéro de la pin où est connecté la LED
 int analogPinCaptor = A0;
 int note = 0x5A; // Note envoyée à chaque depassement de seuil
 int velocity = 0x7F; // Velocité de la note envoyée à chaque depassement de seuil
-int delayNote = 10; // Delay entre deux notes
+int noteLength = 100; // Delay entre deux notes
 
 // Définir la fonction du pgm
 bool isTestMode = false;
@@ -57,54 +56,64 @@ void loop()
     // en Mode TEST Afficher les valeurs
     printResult(result, maxValue);
 
+    // Allumer
+    ledIsOn(true);
+
+    // Attendre
+    delay(noteLength);
+
+    // Eteindre
+    ledIsOn(false);
   } else {
     // Mode pas test -> Mode Midi
     // Verifier seuil
     if (result >= seuil) {
       // Si le resultat est sup à la valeur de seuil
-      // Envoyer note midi
+      // Envoyer note midi et allumer la lumière
       noteOn(0x90, note, velocity); // Note On
-      delay(delayNote);
+      ledIsOn(true);
+
+      delay(noteLength);
+
+      // Envoyer le note off et eteindre la lumiere
       noteOn(0x90, note, 0x00); // Note Off
+      ledIsOn(false);
     }
   }
 
-    // Allumer la led en fonction du resultat et du seuil
-    ledOn(result, seuil, ledTime);
 
-    delay(refreshDelay);
-  }
+  delay(refreshDelay);
+}
 
 
-  void ledOn (int r, int s, int d)
+void ledIsOn (bool isOn)
+{
+  // Allumer la led en fonction du de isOn
+  if (isOn)
   {
-    // Allumer la led en fonction du resultat et du seuil
-    if (r >= s)
-    {
-      digitalWrite(led, HIGH);
-      delay(d);
-    }
-    else
-    {
-      digitalWrite(led, LOW);
-    }
+    digitalWrite(led, HIGH);
   }
-
-
-  void printResult(int v, int m)
+  else
   {
-    // Afficher les Résultats
-    Serial.print("Valeur: ");
-    Serial.print(v);
-    Serial.print(" Max: ");
-    Serial.println(m);
+    digitalWrite(led, LOW);
   }
+}
 
-  //  plays a MIDI note.  Doesn't check to see that
-  //  cmd is greater than 127, or that data values are  less than 127:
-  void noteOn(int cmd, int pitch, int velocity) {
-    Serial.write(cmd);
-    Serial.write(pitch);
-    Serial.write(velocity);
-  }
+
+void printResult(int v, int m)
+{
+  // Afficher les Résultats
+  Serial.print("Valeur: ");
+  Serial.print(v);
+  Serial.print(" Max: ");
+  Serial.println(m);
+}
+
+//  plays a MIDI note.  Doesn't check to see that
+//  cmd is greater than 127, or that data values are  less than 127:
+void noteOn(int cmd, int pitch, int velocity) {
+  Serial.write(cmd);
+  Serial.write(pitch);
+  Serial.write(velocity);
+}
 
